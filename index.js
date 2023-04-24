1f71b4158a21532ebc5297db5bc5a53af76ddd65
aybikeustun <aybikeustun@gmail.com>
2023-04-24 21:45:47 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+function writeCards( names, e) {
+    let newArray = []
+    for( let i = 0; i < names.length; i++ ) {
+      
+        newArray.push( `Thank you, ${names[i]}, for the wonderful ${e} gift!` )
+    
+    
+    }
+    return newArray
+  }
+
+function countDown(num) {
+    while (num > 0) {
+      console.log(num);
+      num--;
+    }
+    console.log(num);
+  }
+  
+
+
+  
